580f33bc05aaba9b8d3e741fc26e557d8736e6e3
Brahmi Youssef (Stagiaire) <y.brahmi@attijariwafa.com>
2024-07-25 11:43:14 +0100

    first commit

---
+++ transformation/src/test/java/com/backendtest/transformation/TransformationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backendtest.transformation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransformationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
